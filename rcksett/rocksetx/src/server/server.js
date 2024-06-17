042b53396c50828cc0e84fdfa79d360a8480ac77
Gabriel erick <102499159+GabrielErick1@users.noreply.github.com>
2024-06-17 11:26:58 -0400

    a

--- rcksett/rocksetx/src/server/server.js
+++
@@ -1,9 +0,0 @@
-import http from 'node:http'
-
-const server = http.createServer((req, res) => {
-  res.
-  setHeader('Content-Type', 'application/json').
-  end('Ola Mundo')
-})
-
-server.listen(8080)
