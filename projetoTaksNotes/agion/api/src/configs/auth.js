9198ea3c77acd773e440c5faf6e9a3001070535f
Gabriel erick <102499159+GabrielErick1@users.noreply.github.com>
2024-07-04 07:02:13 -0400

    a
    a

--- projetoTaksNotes/agion/api/src/configs/auth.js
+++
@@ -1,6 +0,0 @@
-/*export const jwt = {
-  secret: 'default',
-  expiresIn: '2d',
-};*/
-export const secret = 'default';
-export const expiresIn = '2d';
--- projetoTaksNotes/agion/task/src/Components/Buttons/ButtonText.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonstText } from './buttonTextcss.js';
-
-export const ButtonText = ({ title, isActive = false, ...rest }) => {
-  return (
-    <ButtonstText $isactive={isActive} type="button" {...rest}>
-      {title}
-    </ButtonstText>
-  );
-};
