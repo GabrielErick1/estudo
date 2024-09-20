db2bd20ead8a3483fef2ebfbe1df950991f015f1
Gabriel erick <102499159+GabrielErick1@users.noreply.github.com>
2024-09-20 18:27:59 -0400

    a

--- rcksett/rockset21024/gestaoOfcina/prisma/migrations/20240906151048_alterado_data/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterEnum
-ALTER TYPE "TipoFuncionario" ADD VALUE 'funcionario';
-
--- AlterTable
-ALTER TABLE "funcionarios" ADD COLUMN     "telefone" TEXT;
--- rcksett/rockset21024/gestaoOfcina/prisma/migrations/20240907113010_funcionario_date/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "funcionarios" ALTER COLUMN "dataDeNascimento" DROP NOT NULL;
