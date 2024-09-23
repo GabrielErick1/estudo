403dea911d38925d9132d262cc14594248695b69
Gabriel erick <102499159+GabrielErick1@users.noreply.github.com>
2024-09-22 23:23:06 -0400

    a
    a

--- rcksett/rockset21024/gestaoOfcina/src/usecases/users/useCaseAltenticateClient.ts
+++ rcksett/rockset21024/gestaoOfcina/src/usecases/users/useCaseAltenticateClient.ts
@@ -5,20 +5,39 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { AppError } from "@/utils/AppError";
 
-class UsecaseAltenticate {
-   constructor(private repositories: PrismaUserRepository){}
+class UsecaseAuthenticate {
+   constructor(private repositories: PrismaUserRepository) {}
    
-   async execute(data: IRegister): Promise<{ user: RegisterInterface; token: string } | null>  {
-     const user = await this.repositories.FindByEmail(data.email)
-      if(!user){
-        throw new AppError("Usuário não encontrado", 401);
-      }
-     const token = jwt.sign(
-        { id: user.id }, // Payload
-        process.env.JWT_SECRET as string, // Chave secreta
-        { expiresIn: "1h" } // Validade do token
-    );
+   async execute(data: IRegister): Promise<{ user: RegisterInterface; token: string } | null> {
+    
+    let user: RegisterInterface | null = null;
 
-     return { user, token}
-   }
+    if (data.email) {
+        user = await this.repositories.FindByEmail(data.email) as RegisterInterface | null;
+        
+        if (!user) {
+            throw new AppError("E-mail ou senha incorretos.", 409);
+        }
+    }
+    
+    // Verificar a senha utilizando bcrypt, garantindo que user não é null
+    if (user && user.password) {
+        const isPasswordValid = await bcrypt.compare(data.password, user.password);
+        
+        if (!isPasswordValid) {
+            throw new AppError("Login ou senha incorretos.", 409);
+        }
+        
+        // Gerando o token JWT que contém apenas o ID do usuário
+        const token = jwt.sign(
+            { id: user.id }, // Payload
+            process.env.JWT_SECRET as string, // Chave secreta
+            { expiresIn: "1h" } // Validade do token
+        );
+        
+        return { user, token };
+    } else {
+        throw new AppError("Usuário não encontrado.", 404);
+    }
+  }
 }
